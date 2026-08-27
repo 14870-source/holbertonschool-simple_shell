8bae97dbb259868f3984175c39b616e0481b41e6
14870-source <14870@holbertonstudents.com>
2026-08-27 08:13:58 +0000

    Implement simple shell 0.1

--- main.c
+++ main.c
@@ -9,23 +9,53 @@ int main(void)
 {
 	char *line = NULL;
 	size_t size = 0;
+	pid_t pid;
+	char *argv[2];
 
 	while (1)
 	{
-		printf("#cisfun$ ");
-		fflush(stdout);
+		if (isatty(STDIN_FILENO))
+		{
+			printf("#cisfun$ ");
+			fflush(stdout);
+		}
 
 		if (getline(&line, &size, stdin) == -1)
 		{
 			free(line);
-			printf("\n");
+			if (isatty(STDIN_FILENO))
+				printf("\n");
 			return (0);
 		}
 
 		line[strcspn(line, "\n")] = '\0';
 
-		if (line[0] != '\0')
-			execute_command(line);
+		if (line[0] == '\0')
+			continue;
+
+		argv[0] = line;
+		argv[1] = NULL;
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("./hsh");
+			continue;
+		}
+
+		if (pid == 0)
+		{
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror("./hsh");
+				exit(127);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
 
 	return (0);
